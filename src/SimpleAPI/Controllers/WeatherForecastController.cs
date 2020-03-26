f56281d91b36fcf0ec423d26fb12b32c193dbb78
Davide Borghi <dborghi92@gmail.com>
2020-03-26 19:08:48 +0100

    fix test fail

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace SimpleAPI.Controllers
         
         [HttpGet]
         public ActionResult<string> Get(int id)        {
-            return "MAx Power";
+            return "Davide Borghi";
             
         }
 
